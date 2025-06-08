ee68f51773723eeaded4ca911fc90b317bd5aebc
numberforty <hassanm625@gmail.com>
2025-06-08 06:08:54 +0300

    Add Arabic unit aliases plugin

---
+++ plugins/CommunityExtensions/ArabicUnits/ArabicUnits.js
@@ -0,0 +1,82 @@
+// Arabic unit names extension
+// Adds Arabic aliases for common units
+
+numi.addUnit({
+  "id": "meter_ar",
+  "phrases": "متر, مترات",
+  "baseUnitId": "meter",
+  "format": "م",
+  "ratio": 1
+});
+
+numi.addUnit({
+  "id": "centimeter_ar",
+  "phrases": "سنتيمتر, سنتمتر, سم",
+  "baseUnitId": "meter",
+  "format": "سم",
+  "ratio": 0.01
+});
+
+numi.addUnit({
+  "id": "kilometer_ar",
+  "phrases": "كيلومتر, كيلومترات, كم",
+  "baseUnitId": "meter",
+  "format": "كم",
+  "ratio": 1000
+});
+
+numi.addUnit({
+  "id": "gram_ar",
+  "phrases": "غرام, جرام, جرامات",
+  "baseUnitId": "gram",
+  "format": "غ",
+  "ratio": 1
+});
+
+numi.addUnit({
+  "id": "kilogram_ar",
+  "phrases": "كيلوغرام, كيلوجرام, كغ",
+  "baseUnitId": "gram",
+  "format": "كغ",
+  "ratio": 1000
+});
+
+numi.addUnit({
+  "id": "liter_ar",
+  "phrases": "لتر, لترات",
+  "baseUnitId": "liter",
+  "format": "ل",
+  "ratio": 1
+});
+
+numi.addUnit({
+  "id": "milliliter_ar",
+  "phrases": "ملليلتر, ملل",
+  "baseUnitId": "liter",
+  "format": "مل",
+  "ratio": 0.001
+});
+
+numi.addUnit({
+  "id": "second_ar",
+  "phrases": "ثانية, ثواني",
+  "baseUnitId": "second",
+  "format": "ث",
+  "ratio": 1
+});
+
+numi.addUnit({
+  "id": "minute_ar",
+  "phrases": "دقيقة, دقائق",
+  "baseUnitId": "minute",
+  "format": "د",
+  "ratio": 1
+});
+
+numi.addUnit({
+  "id": "hour_ar",
+  "phrases": "ساعة, ساعات",
+  "baseUnitId": "hour",
+  "format": "س",
+  "ratio": 1
+});
